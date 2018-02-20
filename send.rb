3b691a33179ff93c4879fe26398c559933bf634d
jaga810 <salamander.g.1@gmail.com>
2018-02-20 13:06:56 +0000

    [update] send.rb for Frame mode

--- send.rb
+++ send.rb
@@ -14,7 +14,21 @@ sp.read_timeout=1000
 
 pattern=/GNGGA/
 file=File.open("/dev/ttyACM0")
+
 INTERVAL = 5
+
+DEVICE_CONF = {} #deviceid
+
+File.foreach('device.conf') do |text|
+    key = text.split('=')[0]
+    val = text.split('=')[1].chomp
+    DEVICE_CONF[key] = val
+end
+p DEVICE_CONF
+
+PAN_ID = DEVICE_CONF['panid']
+DEVICE_ID = DEVICE_CONF['deviceid']
+NEXT_DEVICE_ID = format("%04x", (DEVICE_ID.to_i(base=16) - 1).to_s(16)).upcase
 
 count = 0
 file.each_line do |text|
@@ -23,7 +37,7 @@ file.each_line do |text|
       a=text.split(",")
       msg=a[1]+","+(a[2].to_f/100.0).to_s+","+(a[4].to_f/100.0).to_s
       pktid = count / INTERVAL
-      sp.write pktid.to_s + ":" + msg + $serial_delimiter
+      sp.write PAN_ID + NEXT_DEVICE_ID + pktid.to_s + ":" + msg + $serial_delimiter
       p sp.gets($serial_delimiter)
     end
     count += 1
