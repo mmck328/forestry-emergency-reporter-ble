c73a207f9938062547887e194859c851dc2aa534
mmck328 <edamame1010@hotmail.co.jp>
2018-02-22 16:19:22 +0900

    [add] logger to send.rb

--- send.rb
+++ send.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'serialport'
+require 'fileutils'
 
 $serial_port = '/dev/ttyUSB0'
 #$serial_port = '/dev/ttyAMA0'
@@ -12,13 +13,7 @@ $serial_delimiter = "\r\n"
 sp = SerialPort.new($serial_port, $serial_baudrate, $serial_databit, $serial_stopbit, $serial_paritycheck)
 sp.read_timeout=10 
 
-pattern=/GNGGA/
-file=File.open("/dev/ttyACM0")
-
-INTERVAL = 5
-
 DEVICE_CONF = {} #deviceid
-
 File.foreach('device.conf') do |text|
     key = text.split('=')[0]
     val = text.split('=')[1].chomp
@@ -26,27 +21,43 @@ File.foreach('device.conf') do |text|
 end
 p DEVICE_CONF
 
+GPS_PATTERN=/GNGGA/
+GPS=File.open("/dev/ttyACM0")
+INTERVAL = 5
+
 PAN_ID = DEVICE_CONF['panid']
 DEVICE_ID = DEVICE_CONF['deviceid']
 NEXT_DEVICE_ID = format("%04X", (DEVICE_ID.to_i(base=16) - 1))
-
 GW_ID = '0000'
 
+FileUtils.makedirs("./send_log")
+LOG_FILENAME = "./send_log/" + Time.now().strftime("%Y%m%d-%H%M%S") + ".txt" 
+LOG_FILE = File.open(LOG_FILENAME, 'a')
+
+def log(text)
+  p text
+  LOG_FILE.puts(text)
+end
+
+def format_gps(text)
+  a = text.split(',')
+  lath, latm = a[2][0..1].to_i, a[2][2..8].to_f
+  lngh, lngm = a[4][0..2].to_i, a[4][3..9].to_f
+  lat = format("%.6f", lath + latm / 60)
+  lng = format("%.6f", lngh + lngm / 60)
+  msg = a[1] + "," + lat + "," + lng
+end
+
 count = 0
-file.each_line do |text|
-  if pattern =~ text
+GPS.each_line do |text|
+  if GPS_PATTERN =~ text
     if count % INTERVAL == 0
-      a = text.split(',')
-      lath, latm = a[2][0..1].to_i, a[2][2..8].to_f
-      lngh, lngm = a[4][0..2].to_i, a[4][3..9].to_f
-      lat = format("%.6f", lath + latm / 60)
-      lng = format("%.6f", lngh + lngm / 60)
-      msg = a[1] + "," + lat + "," + lng
-      
+      msg = format_gps(text)
       pktid = format("%04X", (count / INTERVAL) % 0xffff)
+
       payload = DEVICE_ID + GW_ID + pktid + msg
-      p payload
-      
+
+      log payload
       sp.write PAN_ID + NEXT_DEVICE_ID + payload + $serial_delimiter
     end
     count += 1
@@ -54,7 +65,7 @@ file.each_line do |text|
   loop do
     str = sp.gets($serial_delimiter)
     break unless str 
-    p str
+    log str
   end
 end
 
